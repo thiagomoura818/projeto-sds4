60ce69a6b5891ec1404a94306beda0ba29e57d8d
Thiago Moura <waatsaaynew@gmail.com>
2021-09-10 20:10:31 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/sdsvendas/SdsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sdsvendas/SdsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
